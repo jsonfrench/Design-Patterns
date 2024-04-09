849531af37a46ae7c9395ffbf603b5258a9e49f6
jsonfrench <jsonfrench@gmail.com>
2024-04-09 17:50:33 -0400

    fixed spelling error in comments

--- Adapter_Pattern/DuckToTurkey.java
+++ Adapter_Pattern/DuckToTurkey.java
@@ -13,7 +13,7 @@ public class DuckToTurkey implements Turkey{
     public void fly() {                     //how am i supposed to adapt a duck's flight to a turkey's flight?
         if (rand.nextInt(5)==0){      //i was originally going to have a counter keep track of how many times the fly()   
             duck.fly();                     //method was called, and do fly() every 5th call. 
-        }                                   //this implementation is actually better, because it multiple people are calling the 
+        }                                   //this implementation is actually better, because if multiple people are calling the 
     }                                       //fly() method at the same time, they should get an equal amount of flight
 
     @Override
